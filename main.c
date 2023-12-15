008349885f0a52ee570eedda33bba3066172f2f6
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2023-12-15 09:31:34 -0300

    adding shellsort e quicksort algorithms

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
+
 void bubbleSort(int vet[], int tamanho) {
     int i, j;
     long long int comparacoes = 0; 
@@ -87,16 +88,99 @@ void selectionSort(int vet[], int tamanho) {
     printf("Selection Sort fez %lld comparações e %lld trocas em %.6f segundos.\n", comparacoes, trocas, execution_time);
 }
 
+void quickSort(int Left, int Right, int Vetor[]) {
+    int Aux, Pivor, L, R;
+    long long int comparacoes = 0; 
+    long long int trocas = 0; 
+	clock_t start_time, end_time; 
+
+    L = Left;
+    R = Right;
+    Pivor = Vetor[(Left + Right) / 2];
+    start_time = clock();
+
+    while (L <= R) {
+        while (Vetor[L] < Pivor && L < Right) {
+            L++;
+            comparacoes++; // Incrementa o contador de compara��es
+        }
+        while (Vetor[R] > Pivor && R > Left) {
+            R--;
+            comparacoes++; // Incrementa o contador de compara��es
+        }
+        if (L <= R) {
+            Aux = Vetor[L];
+            Vetor[L] = Vetor[R];
+            Vetor[R] = Aux;
+            L++;
+            R--;
+            trocas += 2; // Incrementa o contador de trocas (2 trocas ocorreram)
+        }
+    }
+
+    if (R > Left) {
+        quickSort(Left, R, Vetor); // Chama recursivamente quickSort com o vetor completo
+    }
+
+    if (L < Right) {
+        quickSort(L, Right, Vetor); // Chama recursivamente quickSort com o vetor completo
+    }
+    end_time = clock();
+
+    double execution_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+
+    printf("Quick Sort fez %lld comparações e %lld trocas em %.6f segundos.\n", comparacoes, trocas, execution_time);
+}
+
+
+void shellSort(int vet[], int tamanho) {
+    int i, j, increment, temp;
+    long long int comparacoes = 0; // contador de compara??es
+    long long int trocas = 0;      // contador de trocas
+    clock_t start_time, end_time;
+
+    start_time = clock();
+
+    increment = 3;
+    while (increment > 0) {
+        for (i = 0; i < tamanho; i++) {
+            j = i;
+            temp = vet[i];
+            while ((j >= increment) && (vet[j - increment] > temp)) {
+                comparacoes++; // incrementa o contador de compara??es
+                vet[j] = vet[j - increment];
+                j = j - increment;
+                trocas++;      // incrementa o contador de trocas
+            }
+            vet[j] = temp;
+        }
+        if (increment / 2 != 0)
+            increment = increment / 2;
+        else if (increment == 1)
+            increment = 0;
+        else
+            increment = 1;
+    }
+
+    end_time = clock();
+
+    double execution_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    printf("Shell Sort fez %lld comparações e %lld trocas em %.6f segundos.\n", comparacoes, trocas, execution_time);
+}
+
+
 void imprimirVetor(int vet[], int tamanho, const char* mensagem) {
+	int i;
     printf("%s:\n", mensagem);
-    for (int i = 0; i < tamanho; i++) {
+    for ( i = 0; i < tamanho; i++) {
         printf("%4d", vet[i]);
     }
     printf("\n\n");
 }
 
 void preencherVetorAleatorio(int vet[], int tamanho) {
-    for (int i = 0; i < tamanho; i++) {
+	int i;
+    for ( i = 0; i < tamanho; i++) {
         vet[i] = rand() % 1000; 
     }
 }
@@ -106,10 +190,10 @@ int main() {
     int tamanhos[] = {100, 1000, 10000, 100000};
 
     printf("Deseja imprimir os vetores antes e depois da execução? (1 - Sim, 0 - Não): ");
-    int opcao;
+    int opcao, k;
     scanf("%d", &opcao);
 
-    for (int k = 0; k < sizeof(tamanhos) / sizeof(tamanhos[0]); k++) {
+    for ( k = 0; k < sizeof(tamanhos) / sizeof(tamanhos[0]); k++) {
         int tamanho = tamanhos[k];
         int vet[tamanho]; 
 
@@ -124,6 +208,9 @@ int main() {
         bubbleSort(vet, tamanho);
         insertionSort(vet, tamanho);
         selectionSort(vet, tamanho);
+        shellSort(vet, tamanho);
+        quickSort(0, tamanho-1, vet);
+        
 
         if (opcao == 1) {
             imprimirVetor(vet, tamanho, "Vetor ordenado");
